01b8a0f89e4049c3a9ad388c34b48d73c749fa09
Nyangolo <nyangoloc@gmail.com>
2023-10-03 01:34:23 -0700

    pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 #Install flask version 2.1.0
-
-
-exec {'pip3 install flask':
-require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+exec { 'install_flask':
+  command  => '/usr/bin/pip3 install flask==2.1.0',
+  unless   => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require  => Exec['python-installed'],
+  creates  => '/usr/local/lib/python3.*/dist-packages/flask',
 }
 
-exec {'python-installed':
-command => '/usr/bin/which python3'
+exec { 'python-installed':
+  command => '/usr/bin/which python3',
 }
