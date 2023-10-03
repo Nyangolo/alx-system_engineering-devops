f74099f62c5e16bff53abe8bdb31c41b1f972250
Nyangolo <nyangoloc@gmail.com>
2023-10-03 01:39:51 -0700

    sudo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,3 @@
-#Install flask version 2.1.0
-exec { 'install_flask':
-  command  => '/usr/bin/pip3 install flask==2.1.0',
-  unless   => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require  => Exec['python-installed'],
-  creates  => '/usr/local/lib/python3.*/dist-packages/flask',
-}
+# install puppet-lint using Puppet
+sudo puppet apply install_flask.pp
 
-exec { 'python-installed':
-  command => '/usr/bin/which python3',
-}
