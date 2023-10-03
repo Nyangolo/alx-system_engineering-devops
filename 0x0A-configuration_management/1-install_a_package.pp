5b744a2c339b8135afd4bfba7914245c498b1687
Nyangolo <nyangoloc@gmail.com>
2023-10-03 01:55:14 -0700

    final

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,29 @@
-# install puppet-lint using Puppet
+#!/usr/bin/env bash
+#install puppet-lint using Puppet
+class { 'python':
+  version => 'system',
+}
 
-sudo puppet apply install_flask.pp
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => [Package['python3-pip'], Class['python']],
+  notify  => Exec['activate_virtualenv'],
+}
+
+exec { 'activate_virtualenv':
+  command     => 'source /path/to/your/virtualenv/bin/activate',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+}
+
+exec { 'check_flask_version':
+  command => 'flask --version',
+  path    => ['/path/to/your/virtualenv/bin', '/usr/bin'],
+  require => Exec['activate_virtualenv'],
+}
